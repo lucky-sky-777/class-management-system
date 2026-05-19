419e77c23cd94788d944f00ac312f79d1b8bb743
Bui_The_Son <buitheson201@gmail.com>
2026-05-19 08:07:33 +0700

    FIX: không hiện tb 403

--- frontend/src/app/providers/index.tsx
+++ frontend/src/app/providers/index.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from '../router';
 import { AppBootstrap } from './AppBootstrap';
+import { ToastContainer } from 'react-toastify';
 
 /**
  * Global application provider that wraps the app with various contexts (Router, Theme, etc.)
@@ -9,6 +10,7 @@ export const AppProvider = () => {
     return (
         <AppBootstrap>
             <RouterProvider router={router} />
+            <ToastContainer />
         </AppBootstrap>
     );
 };
