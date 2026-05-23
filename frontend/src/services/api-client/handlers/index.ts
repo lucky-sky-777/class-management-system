58b1379fa9d8b1f3b1ece5f7482ee4196c2e1184
Bui_The_Son <buitheson201@gmail.com>
2026-05-23 21:21:20 +0700

    fix: xóa handler 404 vì tạm thời k quan trọng, thêm 401 để  xác thực thất bại thì xóa token luon

--- frontend/src/services/api-client/handlers/index.ts
+++ frontend/src/services/api-client/handlers/index.ts
@@ -3,12 +3,12 @@ import { useAuthStore } from "@features/auth/hooks/useAuthStore";
 import { AUTH_STORAGE_KEY } from "@features/auth/types/keyStorage";
 import { toast } from "react-toastify";
 
-export function handler404() {
+export function handler401() {
     storage.remove(AUTH_STORAGE_KEY.TOKEN);
     storage.remove(AUTH_STORAGE_KEY.REFRESH);
     useAuthStore.getState().logout();
 }
 
-export function handler403() {
-    toast.error("bạn không có quyền làm việc này.");
+export function handler403(message?: string | null) {
+    toast.error( message || "bạn không có quyền làm việc này.");
 }
--- frontend/src/services/api-client/interceptors/response.ts
+++ frontend/src/services/api-client/interceptors/response.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
 import type { AxiosResponse } from "axios";
-import { handler403, handler404 } from "@services/api-client/handlers";
+import { handler401, handler403 } from "@services/api-client/handlers";
+import type { ApiResponse } from "@shared/utils/common";
 
-export const responseInterceptor = (response: AxiosResponse): AxiosResponse => {
+export const responseInterceptor = (
+    response: AxiosResponse<ApiResponse<any>>
+): AxiosResponse => {
+    if (response.data.code === 401) { 
+        handler401();
+    }
+    if (response.data.code === 403) {
+        handler403(response.data.message);
+    }
     return response;
 };
 
@@ -14,11 +23,11 @@ export const responseErrorInterceptor = (error: unknown): Promise<never> => {
         return Promise.reject(new Error("Network error: Unable to connect to the server"));
     }
     const data = error.response.data as Record<string, unknown> | undefined;
-    if (error.response.status === 404 || (data && data.code == "404")) {
-        handler404();
+    if (error.response.status === 401 || (data && data.code == "401")) {
+        handler401();
     }
     if (error.response.status === 403 || (data && data.code == "403")) {
-        handler403();
+        handler403(error.response.data?.message as string | null);
     }
     return Promise.reject(error);
 };
