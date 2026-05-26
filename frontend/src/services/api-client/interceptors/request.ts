234f08de6595578a08dda76142e31af8983d23ab
Bui_The_Son <buitheson201@gmail.com>
2026-05-26 13:56:26 +0700

    refreshtokemn

--- frontend/src/services/api-client/interceptors/request.ts
+++ frontend/src/services/api-client/interceptors/request.ts
@@ -1,29 +1,13 @@
 import type { InternalAxiosRequestConfig } from "axios";
 import { storage } from "@shared/storages";
 import { AUTH_STORAGE_KEY } from "@features/auth/types/keyStorage";
-import { jwtDecode } from "jwt-decode";
-import { useAuthStore } from "@features/auth/hooks/useAuthStore";
 
 export const requestInterceptor = (
     config: InternalAxiosRequestConfig
 ): InternalAxiosRequestConfig => {
     const token = storage.get<string>(AUTH_STORAGE_KEY.TOKEN);
     if (token) {
-        try {
-            const decoded = jwtDecode(token);
-            const currentTime = Date.now() / 1000;
-            if (decoded.exp && decoded.exp < currentTime) {
-                storage.remove(AUTH_STORAGE_KEY.TOKEN);
-                storage.remove(AUTH_STORAGE_KEY.REFRESH);
-                useAuthStore.getState().logout();
-            } else {
-                config.headers.Authorization = `Bearer ${token}`;
-            }
-        } catch {
-            storage.remove(AUTH_STORAGE_KEY.TOKEN);
-            storage.remove(AUTH_STORAGE_KEY.REFRESH);
-            useAuthStore.getState().logout();
-        }
+        config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
 };
