7baefdc1480cd90011d2c2ee1873f5e57c50e252
Nguyễn Trần Thiên Bảo <nttb749@gmail.com>
2026-06-07 01:08:31 +0700

    api config

--- frontend/src/services/api-client/config.ts
+++ frontend/src/services/api-client/config.ts
@@ -1,6 +1,6 @@
 import type { CreateAxiosDefaults, AxiosRequestConfig } from "axios";
 
-export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = "https://class-management-system-backend.fly.dev/api";
 
 export const configAxios: CreateAxiosDefaults = {
     baseURL: BASE_URL,
