da874c1cb6118ba11cb73552e4d7658e41f5e4e4
Nguyễn Trần Thiên Bảo <nttb749@gmail.com>
2026-06-16 14:07:52 +0700

    fix origin

--- backend/src/main/java/com/mezon/classmanagement/backend/common/constant/ClientConstant.java
+++ backend/src/main/java/com/mezon/classmanagement/backend/common/constant/ClientConstant.java
@@ -7,7 +7,7 @@ public final class ClientConstant {
 
 	public static final List<String> ALLOWED_ORIGIN_LIST = new ArrayList<>(
 			List.of(
-					"http://localhost:5173",
+					//"http://localhost:5173",
 					"https://iclass.website",
 					"https://class-management-system-frontend.fly.dev"
 			)
